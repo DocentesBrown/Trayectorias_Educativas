26e59eb5ee33c37778b7478682712e7db744ae43
DocentesBrown <docentxsbrown@gmail.com>
2026-02-16 04:01:07 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Pegá acá tu URL del Web App (Apps Script) que termina en /exec
-window.TRAYECTORIAS_BACKEND_URL = "https://script.google.com/macros/s/AKfycbzTmSGwHllhYLtBp5vA046yojWRwk0QwNF4T2Y8m6duK11NKBoA8rnBEH7X4eZ-5vU/exec";
+window.TRAYECTORIAS_BACKEND_URL = "PEGAR_WEB_APP_URL_AQUI";
